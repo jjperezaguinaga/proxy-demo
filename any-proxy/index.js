17766af8aeae0e9052045b9b72f60da91ef4de5b
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2019-09-16 15:11:05 -0500

    Updated anyproxy to be a docker app

--- any-proxy/index.js
+++ any-proxy/index.js
@@ -1,4 +1,10 @@
 const AnyProxy = require('anyproxy');
-const proxyServer = new AnyProxy.ProxyServer({});
+const proxyServer = new AnyProxy.ProxyServer({
+  port: 3000,
+  webInterface: {
+    enable: true,
+    webPort: 3001
+  }
+});
 
 module.exports = proxyServer;
